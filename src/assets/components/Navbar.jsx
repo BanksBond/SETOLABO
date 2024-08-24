7473f39001722e3a6840f0a0db3ba0ea0b5b81b7
Bakhtyar Ansari <bakhtyaransari1@gmail.com>
2024-08-24 20:18:12 +0530

    final

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { AiOutlineMenu } from "react-icons/ai";
+
 // eslint-disable-next-line react/prop-types
 function Navbar({ nav, setNav }) {
   function handleNav(i) {
@@ -8,7 +10,7 @@ function Navbar({ nav, setNav }) {
       <h1 className="kalam-regular cursor-pointer" onClick={() => handleNav(0)}>
         M/M
       </h1>
-      <ul className="flex">
+      <ul className="flex hidden">
         <li
           className={`m-4 cursor-pointer ${nav === 1 ? "active" : ""}`}
           onClick={() => handleNav(1)}
@@ -23,6 +25,16 @@ function Navbar({ nav, setNav }) {
           Booking
         </li>
       </ul>
+      <div>
+        <AiOutlineMenu size={20} />
+      </div>
+      <div className="fixed left-0 top-0 w-[35%] h-full border-r border-r-gray-600 bg-white">
+        <ul className="pt-24 ">
+          <li className="ml-auto mr-auto">Profile</li>
+          <li className="ml-auto mr-auto">Gallery</li>
+          <li className="ml-auto mr-auto">Booking</li>
+        </ul>
+      </div>
     </div>
   );
 }
